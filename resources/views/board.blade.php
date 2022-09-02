aaeca4b4f3306d08fbe8f489238dba3e1d539a0b
Mohamed Elsayed <mohamdeesayed@outlook.com>
2022-09-02 21:39:03 +0200

    update

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -11,24 +11,25 @@
 @section('content')
 
 <div class="row">
+  
     <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
+        <div class="card mx-1">
             <div class="card-body">
               <h5 class="card-title"><a href="{{ route('app.student') }}" class="card-link">student payment</a></h5>
               <h6 class="card-subtitle mb-2 text-muted">this feature for know how upload the bank transaction</h6>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="{{ route('app.student') }}" class="card-link">Card link</a>
+              <p class="card-text">you can access students  data how upload transaction files.</p>
+              <a href="{{ route('app.student') }}" class="card-link">service link</a>
             </div>
           </div>
     </div>
 
     <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
+        <div class="card">
             <div class="card-body">
               <h5 class="card-title"><a href="{{ route('app.student.bus') }}" class="card-link">student Bus</a></h5>
-              <h6 class="card-subtitle mb-2 text-muted">this feature for know how upload the bank transaction</h6>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="{{ route('app.student.bus') }}" class="card-link">Card link</a>
+              <h6 class="card-subtitle mb-2 text-muted">this feature for know how upload the bus transaction</h6>
+              <p class="card-text">you can access students  data how upload transaction files.</p>
+              <a href="{{ route('app.student.bus') }}" class="card-link">service link</a>
             </div>
           </div>
     </div>
