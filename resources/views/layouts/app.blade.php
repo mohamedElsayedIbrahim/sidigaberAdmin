739309116fc2c69fc56d00fe201556ff6903edc1
Sidi Gaber <mohamdeesayed@outlook.com>
2022-09-10 09:56:28 +0200

    fix site icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     @yield('css')  
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="app/public/favicon.ico" type="image/x-icon">
 </head>
   <body>
     
